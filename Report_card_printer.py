1458349a4ac53876aaacdd8e250754d4373d821d
MickeyBioinfor <mickey.chantk@gmail.com>
2026-03-06 12:42:00 +0000

    Add print statements for variable types and values
    Added print statements to display variable types and values.

--- Report_card_printer.py
+++ Report_card_printer.py
@@ -1,3 +1,14 @@
 <h1> Report Card Printer </h1> 
 name = "Mickey"
 # name the variable the value "Mickey"
+
+print(type(name))
+print(name, type(name))
+# print the type of the variable "name"
+
+is_student = True 
+print(is_student, type(is_student)
+# print the value of variable is_student and the type of the value 
+
+age = 20
+print(age, type(age))

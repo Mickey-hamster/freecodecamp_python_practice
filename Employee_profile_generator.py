d20f77ee54949ed77872dc1f9dedd1a397eb37d6
MickeyBioinfor <mickey.chantk@gmail.com>
2026-03-06 14:47:56 +0000

    Add employee profile generation functionality

--- Employee_profile_generator.py
+++ Employee_profile_generator.py
@@ -2,3 +2,17 @@
 
 first_name = "Mickey"
 last_name = "Chan"
+
+full_name = first_name + ' ' + last_name 
+
+address = "123 Main Street" 
+address += ", Apartment 4B"
+
+empolyee_age = 28
+
+employee_info = full_name + " is "  + str(employee_age) + " years old"
+print(employee_info) 
+
+experience_years = 5
+experience_info = "Experience: " + str(experience_years) + " years"
+print(experience_info)
